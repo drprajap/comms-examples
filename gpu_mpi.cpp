b9068a75e73fa527f2bc64b056b7ee7d8928583b
Dimple Prajapati <dimple.prajapati@amd.com>
2025-04-10 14:56:30 -0500

    working mpi example with IPC and formatting

--- gpu_mpi.cpp
+++ gpu_mpi.cpp
@@ -1,89 +1,160 @@
-#include <stdio.h>
+// MIT License
+//
+// Copyright (c) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <hip/hip_runtime.h>
 #include <mpi.h>
+#include <stdio.h>
 
-#include <cstdlib>
-#include <vector>
 #include <cstdio>
-#include <stdexcept>
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+#include <vector>
 
-#define HIP_CHECK(error)          \
-    do {                        \
-        hipError_t err = error;   \
-        if(err != hipSuccess) { \
-            std::cerr << "HIP Error: " << hipGetErrorString(err)    \
-                      << " at " << __FILE__ << ": "                 \
-                      << __LINE__ << std::endl;                      \
-            throw std::runtime_error(hipGetErrorString(err));       \
-        }   \
-        } while(0)
+#define HEAP_SIZE 1 << 30
 
+#define HIP_CHECK(condition)                                                  \
+  {                                                                           \
+    const hipError_t error_code = condition;                                  \
+    if (error_code != hipSuccess) {                                           \
+      std::cerr << "HIP Error encountered: " << hipGetErrorString(error_code) \
+                << " at " << __FILE__ << ": " << __LINE__ << std::endl;       \
+      std::runtime_error(hipGetErrorString(error_code));                      \
+    }                                                                         \
+  }
 
-// Example: IPC memory sharing when all pes are on same node - only works intra-node
-// hipMalloc() to allocate device buffers, 
-// hipIpcGetMemHandle/hipIpcOpenMemHandle to get IPC handle for device buffers
+/// \brief Example for IPC memory sharing via symmetric heap
+/// when all GPU devices are on same node (intra-node)
+/// See collectiveComms.md for more details on intra-node communication via
+/// IPC.
 int main(int argc, char **argv) {
-  int i,rank,size,bufsize;
-  int *h_buf;
-  int *d_buf;
-  MPI_Status status;
-  MPI_Init(&argc,&argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-  printf("Rank: %d CommSize: %d \n", rank, size);
+  int myRank, numRanks;
+  int numDevices, currDevice = 0;
 
+  void *symmetricHeap;
+  size_t heapSize = 1024;
 
-  int hipDeviceCount, currDevice = 0;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);    // Current PE ID
+  MPI_Comm_size(MPI_COMM_WORLD, &numRanks);  // total number of PEs
 
-  HIP_CHECK(hipGetDeviceCount(&hipDeviceCount));
-  currDevice = rank % hipDeviceCount;
+  HIP_CHECK(hipGetDeviceCount(&numDevices));
+  currDevice = myRank % numDevices;
   HIP_CHECK(hipSetDevice(currDevice));
 
-  printf("HIP Device count: %d , CurrDevice: %d\n", hipDeviceCount, currDevice);
+  printf("CommSize: %d HIP Device count: %d , CurrDevice: %d Rank: %d\n",
+         numRanks, numDevices, currDevice, myRank);
 
-  bufsize=100;
+  HIP_CHECK(hipMalloc(&symmetricHeap, /*HEAP_SIZE*(sizeof(char))*/ heapSize));
+  HIP_CHECK(hipMemset(symmetricHeap, currDevice, heapSize));
 
-  int hipSize = 1 << 30;
+  void **heapBases = (void **)malloc(numDevices * sizeof(void *));
+  heapBases[currDevice] = symmetricHeap;
 
-  //allocate buffers
-  h_buf=(int*) malloc(sizeof(int)*bufsize);
-  hipMalloc(&d_buf, bufsize*sizeof(int));
+  printf("heapBases[%d]: %p \n", currDevice, heapBases[currDevice]);
 
-  //initialize buffers
-  if(rank==0) {
-    for(i=0;i<bufsize;i++)
-      h_buf[i]=i;
+  hipIpcMemHandle_t ipc_handle;
+
+  // Gets IPC memory handle for local device's symmetric heap that can be given
+  // to hipIpcOpenMemHandle, that maps this heap allocation to another device's
+  // address space.
+  HIP_CHECK(hipIpcGetMemHandle(&ipc_handle, symmetricHeap));
+
+  hipIpcMemHandle_t *ipcMemHandles =
+      (hipIpcMemHandle_t *)malloc(numDevices * sizeof(hipIpcMemHandle_t));
+  MPI_Allgather(&ipc_handle, sizeof(hipIpcMemHandle_t), MPI_CHAR, ipcMemHandles,
+                sizeof(hipIpcMemHandle_t), MPI_CHAR, MPI_COMM_WORLD);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  void *remoteBuf = NULL;
+  HIP_CHECK(hipMalloc(&remoteBuf, heapSize));
+
+  // Open IpcHandle associated with symmetric heap allocation on remote/peer
+  // device and maps allocation memory to current device address space,
+  // current device can use/access the remote buffer by referencing device
+  // pointer returned by hipIpcOpenMemHandle.
+  int peerDevice = (currDevice + 1) % 2;
+  if (currDevice == 0) {
+    HIP_CHECK(hipIpcOpenMemHandle(&remoteBuf, ipcMemHandles[peerDevice],
+                                  hipIpcMemLazyEnablePeerAccess));
+    printf("GPU: %d - heap from GPU 1: %p\n", currDevice, remoteBuf);
+  } else if (currDevice == 1) {
+    HIP_CHECK(hipIpcOpenMemHandle(&remoteBuf, ipcMemHandles[peerDevice],
+                                  hipIpcMemLazyEnablePeerAccess));
+    printf("GPU: %d - heap from GPU 0: %p\n", currDevice, remoteBuf);
   }
+  MPI_Barrier(MPI_COMM_WORLD);
 
-  if(rank==1) {
-    for(i=0;i<bufsize;i++)
-      h_buf[i]=-1;
-  }
+  char *hostBuf = (char *)malloc(heapSize);
+  HIP_CHECK(hipMemcpy(hostBuf, remoteBuf, heapSize, hipMemcpyDeviceToHost));
 
-  hipMemcpy(d_buf, h_buf, bufsize*sizeof(int), hipMemcpyHostToDevice);
+  char *hostLocalBuf = (char *)malloc(heapSize);
+  HIP_CHECK(
+      hipMemcpy(hostLocalBuf, symmetricHeap, heapSize, hipMemcpyDeviceToHost));
 
-  //communication
-  if(rank==0)
-    MPI_Send(d_buf, bufsize, MPI_INT, 1, 123, MPI_COMM_WORLD);
+  printf("GPU: %d - data from local buf : %d %d %d\n", currDevice,
+         hostLocalBuf[0], hostLocalBuf[1], hostLocalBuf[2]);
 
-  if(rank==1)
-    MPI_Recv(d_buf, bufsize, MPI_INT, 0, 123, MPI_COMM_WORLD, &status);
+  printf("GPU: %d - data from Remote buf : %d %d %d\n", currDevice, hostBuf[0],
+         hostBuf[1], hostBuf[2]);
 
-  //validate results
-  if(rank==1) {
-    hipMemcpy(h_buf, d_buf, bufsize*sizeof(int), hipMemcpyDeviceToHost);
-    printf("buffer Data from rank 1: %d \n", h_buf[95]);
-    for(i=0;i<bufsize;i++) {
-      if(h_buf[i] != i)
-        printf("Error: buffer[%d]=%d but expected %d\n", i, h_buf[i], i);
-      }
-    fflush(stdout);
-  }
+  HIP_CHECK(hipIpcCloseMemHandle(remoteBuf));
 
-  //free buffers
-  free(h_buf);
-  hipFree(d_buf);
-	
+  // char *baseHeap = (char *) heapBases[myRank];
+  // printf("baseHeap: %x\n", baseHeap);
+  // HIP_CHECK(hipIpcGetMemHandle(&ipcMemHandles[myRank], baseHeap));
+
+  // MPI_Barrier(MPI_COMM_WORLD);
+
+  // printf("ipcHandle: %p\n", ipcMemHandles[myRank]);
+  // MPI_Comm shmcomm;
+  // MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL,
+  //                     &shmcomm);
+  // MPI_Allgather(MPI_IN_PLACE, sizeof(hipIpcMemHandle_t), MPI_CHAR,
+  // ipcMemHandles, sizeof(hipIpcMemHandle_t),MPI_CHAR, shmcomm);
+
+  // char **ipcBase;
+  // HIP_CHECK(hipMalloc(reinterpret_cast<void **>(&ipcBase),
+  //             numRanks * sizeof(char **)));
+
+  // for (int i = 0; i < numRanks; i++) {
+  //   if (i != myRank) {
+  //     void **ipc_base_uncast = reinterpret_cast<void **>(&ipcBase[i]);
+  //     HIP_CHECK(hipIpcOpenMemHandle(ipc_base_uncast, ipcMemHandles[i],
+  //                                   hipIpcMemLazyEnablePeerAccess));
+  //   } else {
+  //     ipcBase[i] = (char *)heapBases[myRank];
+  //   }
+  // }
+  // MPI_Barrier(MPI_COMM_WORLD);
+
+  HIP_CHECK(hipFree(symmetricHeap));
+  free(hostBuf);
+  free(hostLocalBuf);
+  free(ipcMemHandles);
+  free(heapBases);
+
   MPI_Finalize();
+
+  return 0;
 }
