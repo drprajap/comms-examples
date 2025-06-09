0af3c58d156bb39b30a44b92fcfaa797d43a679d
Dimple Prajapati <dimple.prajapati@amd.com>
2025-06-09 22:17:06 +0000

    add compile/link/run commands

--- rocshmem_test.cc
+++ rocshmem_test.cc
@@ -1,3 +1,39 @@
+// MIT License
+//
+// Copyright (c) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+/*
+Compile:
+hipcc -c -fgpu-rdc -x hip rocshmem_test.cc -I${ROCM_PATH}/include  \
+-I${ROSHMEM_INSTALL_DIR}/include -I${OMPI_DIR}/include
+
+Link:
+hipcc -fgpu-rdc --hip-link rocshmem_test.o -o rocshmem_test         \
+${ROSHMEM_INSTALL_DIR}/lib/librocshmem.a  ${OMPI_DIR}/lib/libmpi.so \
+-L${ROCM_PATH}/lib -lamdhip64 -lhsa-runtime64
+
+Run using mpirun:
+${OMPI_DIR}/bin/mpirun -np 8 --allow-run-as-root ./rocshmem_test
+*/
+
 #include <hip/hip_runtime.h>
 #include <hip/hip_runtime_api.h>
 #include <stdio.h>
